bc0dd6686a23a890ebb3c1d1671f9a389eae7737
gentsadiku21 <gs55392@ubt-uni.net>
2024-01-08 00:41:54 +0200

    Added a slider at about us page (Rreth nesh ) and changed css
    changed css because the other one was not leting the slider work

--- RrethNesh.php
+++ RrethNesh.php
@@ -1,34 +1,33 @@
 <?php 
 session_start();
 
-	include("connection.php");
-	include("functions.php");
+include("connection.php");
+include("functions.php");
 
-	$user_data = check_login($con);
-    if($_SERVER['REQUEST_METHOD'] == "POST")
-	{
-		//something was posted
-		$Firstname=$_POST['Firstname'];
-		$Lastname=$_POST['Lastname'];
-		 $Email = $_POST['Email'];
-		$Koment = $_POST['Koment'];
+$user_data = check_login($con);
+if($_SERVER['REQUEST_METHOD'] == "POST")
+{
+    //something was posted
+    $Firstname=$_POST['Firstname'];
+    $Lastname=$_POST['Lastname'];
+     $Email = $_POST['Email'];
+    $Koment = $_POST['Koment'];
 
-    if( !empty($Firstname) && !empty($Lastname) &&!empty($Email) && !empty($Koment))
-		{
+if( !empty($Firstname) && !empty($Lastname) &&!empty($Email) && !empty($Koment))
+    {
 
-			//save to database
-			$user_id = random_num(20);
-			$query = "insert into kontakt (Firstname,Lastname,Email,Koment) values ('$Firstname','$Lastname','$Email','$Koment')";
+        //save to database
+        $user_id = random_num(20);
+        $query = "insert into kontakt (Firstname,Lastname,Email,Koment) values ('$Firstname','$Lastname','$Email','$Koment')";
 
-			mysqli_query($con, $query);
-        }
-		else
-		{
-			echo "Please enter some valid information!";
-	   }
-	
-	}
+        mysqli_query($con, $query);
+    }
+    else
+    {
+        echo "Please enter some valid information!";
+   }
 
+}
 ?>
 
 <!DOCTYPE html>
@@ -39,12 +38,12 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"> 
-    <link rel="stylesheet" href="RrethNesh.css">
+    <link rel="stylesheet" href="AboutUs.css">
     <title>Dhuro Gjak</title>
 </head>
 <body>
-   
-        <div class="wrapper">
+
+<div class="wrapper">
            <nav class="nav">
                <div class="nav-logo">
                    <p> Dhuro Gjak</p>
@@ -89,20 +88,20 @@ session_start();
                 <div class="two-forms">
 
                     <div class="input-box">
-                        <input type="text" id="Firstname" class="input-field" placeholder="Emri" name="Firstname">
+                        <input type="text" id="Firstname" class="input-field" placeholder="Emri" name="Firstname" required>
                     </div>
 
                     <div class="input-box">
-                        <input type="text" id="Lastname" class="input-field" placeholder="Mbiemri" name="Lastname">
+                        <input type="text" id="Lastname" class="input-field" placeholder="Mbiemri" name="Lastname" required>
                      </div>
                 </div>
 
                 <div class="input-box">
-                    <input type="text" id="Email" class="input-field" placeholder="Email" name="Email">
+                    <input type="text" id="Email" class="input-field" placeholder="Email" name="Email" required>
                 </div>
 
                 <div class="input-box">
-                    <textarea type="text" id="Koment" class="input-field" rows="5" placeholder="Shkruani mesazhin tuaj!" name="Koment"></textarea>
+                    <textarea type="text" id="Koment" class="input-field" rows="5" placeholder="Shkruani mesazhin tuaj!" name="Koment" required></textarea>
                 </div>
 
                 <div class="input-box">
@@ -114,22 +113,70 @@ session_start();
            </div>
        </div>
     </section>
-<br>
-       
 
-              <footer class="footer">
-            <div class="container">
-    
-              <p class="copyright">
-                &copy; 2023 All Rights Reserved by <a href="#" class="copyright-link">Dhuro Gjake</a>
-              </p>
-        
-            </div> 
-          </footer>
+<div class="slider">
+        <div class="images">
+        <img src="Foto/Slider5.jpg" alt="Image 5">
+            <img src="Foto/Slider1.jpg" alt="Image 1">
+            <img src="Foto/Slider2.jpg" alt="Image 2">
+            <img src="Foto/Slider3.jpg" alt="Image 3">
+            <img src="Foto/Slider4.jpg" alt="Image 4">
+        </div>
+        <div class="dots">
+        <label for="img5"></label>
+            <label for="img1"></label>
+            <label for="img2"></label>
+            <label for="img3"></label>
+            <label for="img4"></label>
+        </div>
+    </div>
 
-          <script>
-   
-            function myMenuFunction() {
+    <footer class="footer">
+
+<div class="container">
+  <p class="copyright">
+    &copy; 2023 All Rights Reserved by <a href="#" class="copyright-link">Dhuro Gjake</a>
+  </p>
+</div> 
+
+</footer>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            var slideIndex = 0;
+
+            function showSlides(n) {
+                var slides = $('.images');
+                var slideWidth = $('.images img').width();
+
+                slides.css('transform', 'translateX(' + (-slideWidth * n) + 'px)');
+            }
+
+            $('.dots label').on('click', function () {
+                var index = $(this).index();
+                slideIndex = index;
+                showSlides(slideIndex);
+            });
+
+            function slideShow() {
+                slideIndex++;
+                if (slideIndex >= $('.images img').length) {
+                    slideIndex = 0;
+                }
+                showSlides(slideIndex);
+            }
+
+            var slideInterval = setInterval(slideShow, 3000);
+
+            $('.slider').mouseenter(function () {
+                clearInterval(slideInterval);
+            }).mouseleave(function () {
+                slideInterval = setInterval(slideShow, 3000);
+            });
+        });
+          
+        function myMenuFunction() {
              var i = document.getElementById("navMenu");
              if(i.className === "nav-menu") {
                  i.className += " responsive";
@@ -140,8 +187,7 @@ session_start();
         /*  Javascript per me bo hide navbar kur jem scrool posht*/
 
             let lastScrollTop = 0;
-
-window.addEventListener('scroll', function() {
+     window.addEventListener('scroll', function() {
     let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
 
     if (currentScroll > lastScrollTop) {
@@ -153,8 +199,8 @@ window.addEventListener('scroll', function() {
     }
     lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; 
 }, false);
-
 /*==========================================================================*/
-  </script>
+
+</script>
 </body>
 </html>
