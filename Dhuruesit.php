275302d1d42a252be82fc0d20607dfa8a0cc2402
gentsadiku21 <gs55392@ubt-uni.net>
2024-01-17 20:09:07 +0200

    Changed dhuruesit page to display all dhurues from database

--- Dhuruesit.php
+++ Dhuruesit.php
@@ -1,16 +1,17 @@
-<?php 
+<?php
 session_start();
 
-	include("connection.php");
-	include("functions.php");
+include("connection.php");
+include("functions.php");
 
-	$user_data = check_login($con);
+$user_data = check_login($con);
 
+// Write the query to get data from Dhuruesit table
+$sql = "SELECT * FROM dhuruesit";
+
+// Execute the query
+$result = $con->query($sql);
 ?>
-
-
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -23,294 +24,149 @@ session_start();
     <title>Dhuro Gjak</title>
 </head>
 <body>
-   
-        <div class="wrapper">
-           <nav class="nav">
-               <div class="nav-logo">
-                   <p> Dhuro Gjak</p>
-               </div>
-               <div class="nav-menu" id="navMenu">
-                   <ul>
-                       <li><a href="ballina.php" class="link  ">Ballina</a></li>
-                       <li><a href="Dhuruesit.php" class="link active">Dhuruesit</a></li>
-                       <li><a href="Kerkuesit.php" class="link">Kerkuesit</a></li>
-                       <li><a href="RrethNesh.php" class="link">Rreth Nesh</a></li>
-                   
-                       <li><a href="logout.php" class="link ">logout</a></li>
-                   </ul>
-               </div>
-            
-               <div class="nav-menu-btn">
-                   <i class="bx bx-menu" onclick="myMenuFunction()"></i>
-               </div>
-           </nav>
-        </div>
-<br>
-<br>
-<br>
-<br>
-<br>
-
-      
-<!----------------------------------------------------------------------->
-<div class="courses-container">
-	<div class="course">
-		<div class="course-preview">
-			<div class="container-1">
-                <div class="box-1">
-                
-                    <p>
-                    <b>
-                    Për të gjetur se cilit grup mund ti dhuroni gjak-Selektoni grupin e gjakut tuaj :   
-                    </b>
-                    
-                    </p>
-                
-                                   <br>
-             &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<button type="button" onclick="a_pozitiv()">A+</button>
-             &nbsp&nbsp&nbsp&nbsp&nbsp<button type="button" onclick="a_negativ()">A-</button>
-              &nbsp&nbsp&nbsp&nbsp&nbsp<button type="button" onclick="b_pozitiv()">B+</button>
-             &nbsp&nbsp&nbsp&nbsp&nbsp<button type="button" onclick="b_negativ()">B-</button>
-             &nbsp&nbsp&nbsp&nbsp&nbsp<button type="button" onclick="ab_pozitiv()">AB+</button>
-             &nbsp&nbsp&nbsp&nbsp&nbsp<button type="button" onclick="ab_negativ()">AB-</button>
-            &nbsp&nbsp&nbsp&nbsp&nbsp<button type="button" onclick="o_pozitiv()">O+</button>
-            &nbsp&nbsp&nbsp&nbsp&nbsp<button type="button" onclick="o_negativ()">O-</button>
-                                   <br>
-                                   <br>
-             <p id="demo"> Ju lutem shtypni njërin nga butonet:</p>                
-                 </div>
-                 </div> 
-		</div>
-	</div>
-</div>
-
-
-
-<!---------------------------------------------->
-<section id="section1">
-<div class="container">
-<div class="card card-1">
-
-    <!-- card-header -->
-    <div class="card-header">
-      <img src="Foto/Dhuruesi1.jpg" class="card-img" />
+    <div class="wrapper">
+        <nav class="nav">
+            <div class="nav-logo">
+                <p> Dhuro Gjak</p>
+            </div>
+            <div class="nav-menu" id="navMenu">
+                <ul>
+                    <li><a href="ballina.php" class="link">Ballina</a></li>
+                    <li><a href="Dhuruesit.php" class="link active">Dhuruesit</a></li>
+                    <li><a href="Kerkuesit.php" class="link">Kerkuesit</a></li>
+                    <li><a href="RrethNesh.php" class="link">Rreth Nesh</a></li>
+                    <li><a href="logout.php" class="link">logout</a></li>
+                </ul>
+            </div>
+            <div class="nav-menu-btn">
+                <i class="bx bx-menu" onclick="myMenuFunction()"></i>
+            </div>
+        </nav>
     </div>
-    
-    <!------->
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
 
-    <!-- card-body -->
-    <div class="card-body">
-      
-      <h2 class="card-title">Anesa Hoti</h2>
-      <p class="card-text">
-         Grupi Gjakut: AB+ <br><br>
-     Dhurues i Rregullt: 2+ vite <br><br>
-     Qyteti: Gjilan
-
-      </p>
-    </div>
-   
-  
-  </div>
-  <!-- Card 1 -->
-
-
-  <!-- Card 2 -->
-  <div class="card card-2">
-    <!-- card-header -->
-    <div class="card-header">
-      <img src="Foto/Dhuruesi2.jpg" class="card-img" />
-    </div>
-    <!-- card-header -->
-
-    <!-- card-body -->
-    <div class="card-body">
-     
-      <h4 class="card-title">Agon-Besa Krasniqi</h4>
-      <p class="card-text">
-         Grupi Gjakut: AB+ , B- <br><br>
-         Dhurues Të Rregullt: 4+ vite <br><br>
-         Qyteti: Prishtinë
-    
-      </p>
-    </div>
-    <!-- card-body -->
-
-  
-   
-  </div>
-  <!-- Card 2 -->
-
-  <!-- Card 3 -->
-  <div class="card card-3">
-    <!-- card-header -->
-    <div class="card-header">
-      <img src="Foto/Dhuruesi3.jpg" class="card-img" />
-    </div>
-    <!-- card-header -->
-
-    <!-- card-body -->
-    <div class="card-body">
-     
-      <h2 class="card-title">Tringa Jashari</h2>
-      <p class="card-text">
-        Grupi Gjakut: A+ <br><br>
-         Dhurues i Rregullt: 2+ vite<br><br>
-         Qyteti: Pejë
-    
-      </p>
-    </div>
-    <!-- card-body -->
-
-   
-  
-    <!-- card-footer -->
-  </div>
-</div>
-</section>
-
-<section id="section1">
-    <div class="container">
-    <div class="card card-1">
-    
-        <!-- card-header -->
-        <div class="card-header">
-          <img src="Foto/Dhuruesi4.jpg" class="card-img" />
+    <!----------------------------------------------------------------------->
+    <div class="courses-container">
+        <div class="course">
+            <div class="course-preview">
+                <div class="container-1">
+                    <div class="box-1">
+                        <p>
+                            <b>
+                                Për të gjetur se cilit grup mund ti dhuroni gjak-Selektoni grupin e gjakut tuaj :
+                            </b>
+                        </p>
+                        <br>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button type="button"
+                            onclick="a_pozitiv()">A+</button>
+                        &nbsp;&nbsp;&nbsp;&nbsp;<button type="button" onclick="a_negativ()">A-</button>
+                        &nbsp;&nbsp;&nbsp;&nbsp;<button type="button" onclick="b_pozitiv()">B+</button>
+                        &nbsp;&nbsp;&nbsp;&nbsp;<button type="button" onclick="b_negativ()">B-</button>
+                        &nbsp;&nbsp;&nbsp;&nbsp;<button type="button" onclick="ab_pozitiv()">AB+</button>
+                        &nbsp;&nbsp;&nbsp;&nbsp;<button type="button" onclick="ab_negativ()">AB-</button>
+                        &nbsp;&nbsp;&nbsp;&nbsp;<button type="button" onclick="o_pozitiv()">O+</button>
+                        &nbsp;&nbsp;&nbsp;&nbsp;<button type="button" onclick="o_negativ()">O-</button>
+                        <br>
+                        <br>
+                        <p id="demo"> Ju lutem shtypni njërin nga butonet:</p>
+                    </div>
+                </div>
+            </div>
         </div>
-        
-        <!------->
-    
-        <!-- card-body -->
-        <div class="card-body">
-          
-          <h2 class="card-title">Alban Kosumi</h2>
-          <p class="card-text">
-             Grupi Gjakut: 0+ <br><br>
-            Dhurues i Rregullt: 3+ vite<br><br>
-            Qyteti: Mitrovicë
-       
-          </p>
-        </div>
-     
-       
-      </div>
-      <!-- Card 1 -->
-    
-    
-      <!-- Card 2 -->
-      <div class="card card-2">
-        <!-- card-header -->
-        <div class="card-header">
-          <img src="Foto/Dhuruesi5.jpg" class="card-img" />
-        </div>
-        <!-- card-header -->
-    
-        <!-- card-body -->
-        <div class="card-body">
-         
-          <h2 class="card-title">Enkeleda Gashi</h2>
-          <p class="card-text">
-             Grupi Gjakut: B+ <br><br>
-             Dhurues i Rregullt: 5+ vite<br><br>
-             Qyteti: Istog
-          </p>
-        </div>
-        <!-- card-body -->
-      </div>
-      <!-- Card 2 -->
-    
-      
-      <!-- Card 3 -->
-      <div class="card card-3">
-        <!-- card-header -->
-        <div class="card-header">
-          <img src="Foto/Dhuruesi6.jpg" class="card-img" />
-        </div>
-        <!-- card-header -->
-    
-        <!-- card-body -->
-        <div class="card-body">
-         
-          <h2 class="card-title">Trim Ademi</h2>
-          <p class="card-text">
-              Grupi Gjakut: B- <br><br>
-              Dhurues i Rregullt: 2+ vite<br><br>
-              Qyteti: Prizeren
-
-          </p>
-        </div>
-        <!-- card-body -->
-  
-      </div>
     </div>
-    </section>
-<!------------------------------------------------------------------------------->
-          <footer class="footer">
-            <div class="container">
-        
-              <p class="copyright">
-                &copy; 2023 All Rights Reserved by <a href="#" class="copyright-link">Dhuro Gjake</a>
-              </p>
-        
-            </div> 
-          </footer>
 
-
-        <script>
-   
-            function myMenuFunction() {
-             var i = document.getElementById("navMenu");
-             if(i.className === "nav-menu") {
-                 i.className += " responsive";
-             } else {
-                 i.className = "nav-menu";
-             }
+    <!---------------------------------------------->
+    <section id="section1">
+        <div class="container">
+            <?php
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    echo '<div class="card">';
+                    echo '<div class="card-header">';
+                    echo '<img src="data:image/jpeg;base64,' . base64_encode($row['image']) . '" class="card-img" />';
+                    echo '</div>';
+                    echo '<div class="card-body">';
+                    echo '<h2 class="card-title">' . $row['Emri'] . ' ' . $row['Mbiemri'] . '</h2>';
+                    echo '<p class="card-text">';
+                    echo 'Nr-Telefonit: ' . $row['Nrtelefonit'] . '<br>','<br>';
+                    echo 'Grupi-Gjakut: ' . $row['GrupiGjakut'] . '<br>','<br>';
+                    echo 'Qyteti: ' . $row['Qyteti'] . '<br>';
+                    echo '</p>';
+                    echo '</div>';
+                    echo '</div>';
+                }
             }
-        /*  Javascript per me bo hide navbar kur jem scrool posht*/
+            ?>
+        </div>
+    </section>
 
-            let lastScrollTop = 0;
+    <!------------------------------------------------------------------------------->
+    <footer class="footer">
+        <div class="container">
+            <p class="copyright">
+                &copy; 2023 All Rights Reserved by <a href="#" class="copyright-link">Dhuro Gjake</a>
+            </p>
+        </div>
+    </footer>
 
-window.addEventListener('scroll', function() {
-    let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+    <script>
+        function myMenuFunction() {
+            var i = document.getElementById("navMenu");
+            if (i.className === "nav-menu") {
+                i.className += " responsive";
+            } else {
+                i.className = "nav-menu";
+            }
+        }
 
-    if (currentScroll > lastScrollTop) {
-        // Kur Bojm Scrool down
-        document.querySelector('.nav').classList.add('nav-hidden');
-    } else {
-        // Kur Bojm Scroll up
-        document.querySelector('.nav').classList.remove('nav-hidden');
-    }
-    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; 
-}, false);
+        let lastScrollTop = 0;
 
+        window.addEventListener('scroll', function () {
+            let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
 
-/* javascript per grupet e gjakut*/
+            if (currentScroll > lastScrollTop) {
+                document.querySelector('.nav').classList.add('nav-hidden');
+            } else {
+                document.querySelector('.nav').classList.remove('nav-hidden');
+            }
+            lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+        }, false);
 
-                                function a_pozitiv() {
-                               document.getElementById("demo").innerHTML = "Ju mund ti dhuroni grupeve: A+ dhe AB+ Dhe mund të merrni nga:A+, A-, O+ dhe O-";
-                               }
-                               function a_negativ() {
-                               document.getElementById("demo").innerHTML = "Ju mund ti dhuroni grupeve:A-, A+, AB- dhe AB+ Dhe mund të merrni nga:A- dhe O-";
-                               }
-                               function b_pozitiv() {
-                               document.getElementById("demo").innerHTML = "Ju mund ti dhuroni grupeve:B+ dhe AB+ Dhe mund të merrni nga:B+, B-, O+ dhe O-";
-                               }
-                               function b_negativ() {
-                               document.getElementById("demo").innerHTML = "Ju mund ti dhuroni grupeve:AB-, AB+, B- dhe B+ Dhe mund të merrni nga:B- dhe O-";
-                               }
-                               function ab_pozitiv() {
-                               document.getElementById("demo").innerHTML = "Ju mund ti dhuroni grupeve:AB+ Dhe mund te merrni nga të gjitha grupet";
-                               }
-                               function ab_negativ() {
-                               document.getElementById("demo").innerHTML = "Ju mund ti dhuroni grupeve:AB+ dhe AB- Dhe mund të merrni nga:AB+, A-, B- dhe O-";
-                               }
-                               function o_pozitiv() {
-                               document.getElementById("demo").innerHTML = "Ju mund ti dhuroni grupeve:O+, A+, B+ dhe AB+ Dhe mund të merrni nga:O+ dhe O-";
-                               }
-                               function o_negativ() {
-                               document.getElementById("demo").innerHTML = "Ju mund ti dhuroni the gjitha grupeve Dhe mund të merrni vetem nga O-";
-                               }
+        function a_pozitiv() {
+            document.getElementById("demo").innerHTML = "Ju mund ti dhuroni grupeve: A+ dhe AB+ Dhe mund të merrni nga:A+, A-, O+ dhe O-";
+        }
 
-         </script>
+        function a_negativ() {
+            document.getElementById("demo").innerHTML = "Ju mund ti dhuroni grupeve:A-, A+, AB- dhe AB+ Dhe mund të merrni nga:A- dhe O-";
+        }
+
+        function b_pozitiv() {
+            document.getElementById("demo").innerHTML = "Ju mund ti dhuroni grupeve:B+ dhe AB+ Dhe mund të merrni nga:B+, B-, O+ dhe O-";
+        }
+
+        function b_negativ() {
+            document.getElementById("demo").innerHTML = "Ju mund ti dhuroni grupeve:AB-, AB+, B- dhe B+ Dhe mund të merrni nga:B- dhe O-";
+        }
+
+        function ab_pozitiv() {
+            document.getElementById("demo").innerHTML = "Ju mund ti dhuroni grupeve:AB+ Dhe mund te merrni nga të gjitha grupet";
+        }
+
+        function ab_negativ() {
+            document.getElementById("demo").innerHTML = "Ju mund ti dhuroni grupeve:AB+ dhe AB- Dhe mund të merrni nga:AB+, A-, B- dhe O-";
+        }
+
+        function o_pozitiv() {
+            document.getElementById("demo").innerHTML = "Ju mund ti dhuroni grupeve:O+, A+, B+ dhe AB+ Dhe mund të merrni nga:O+ dhe O-";
+        }
+
+        function o_negativ() {
+            document.getElementById("demo").innerHTML = "Ju mund ti dhuroni the gjitha grupeve Dhe mund të merrni vetem nga O-";
+        }
+    </script>
 </body>
 </html>
